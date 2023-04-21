0bdeab4f160853e4bd8a9379b8b636fcd24f5304
alexandyan <alyan25@amherst.edu>
2023-04-20 20:31:27 -0400

    Wrangle 2020 clusters death + freedom

---
+++ cluster-wrangling.R
@@ -0,0 +1,72 @@
+library(tidyverse)
+library(kableExtra)
+library(ggrepel)
+library(broom)
+library(GGally)
+
+# Alex Yan and Lily Wickland Shearer
+# data from shiny project
+scatterplot_data <- readRDS("data/scatterplot_data_without_2019.Rds")
+
+scatter_scaled <- scatterplot_data %>%
+  mutate(across(where(is.numeric),  ~scale(.)[,1], .names = "{.col}_scaled")) %>%
+  select(Country, year, `Happiness score_scaled`, `Freedom to make life choices_scaled`, 
+         tot_year_deaths_scaled, tot_year_cases_scaled) %>%
+  drop_na()
+
+scatter_scaled_2020 <- filter(scatter_scaled, year == 2020) %>%
+  select(-Country, -year) %>%
+  drop_na()
+
+scatter_2020 <- filter(scatterplot_data, year == 2020) %>%
+  select(-year) %>%
+  drop_na()
+
+set.seed(23)
+
+# generate elbow plot data
+elbow_plot <- tibble(k = 1:10) %>%
+  mutate(
+    kmeans_results = purrr::map(k, ~kmeans(scatter_scaled_2020, .x)),
+    # List-column of "glanced" model summaries for each kmeans object
+    # (apply `glance()` to each corresponding result after running `kmeans()`)
+    glanced = purrr::map(kmeans_results, glance)) %>% 
+  # Turn `glanced` list-column into regular tibble columns
+  unnest(cols = c(glanced))
+
+# Construct elbow plot
+ggplot(elbow_plot, aes(x = k, y = tot.withinss)) +
+  geom_point() + 
+  geom_line() +
+  scale_x_continuous(breaks = 1:10) +
+  labs(x = "Number of clusters (k)", 
+       y = "Total within-cluster sum of squares")
+ggsave("img/elbow.png")
+
+scatter_2020_kmeans4 <- scatter_scaled_2020 %>% 
+  kmeans(centers = 4, nstart = 20)
+
+scatter_2020_kmeans4_summary <- tidy(scatter_2020_kmeans4) 
+names(scatter_2020_kmeans4_summary) = gsub(pattern = "_scaled", replacement = "", x = names(scatter_2020_kmeans4_summary))
+
+scatter_2020_kmeans4_country <- augment(scatter_2020_kmeans4, scatter_2020)
+
+ggplot(scatter_2020_kmeans4_country, aes(x = tot_year_deaths, y = get('Freedom to make life choices'))) + 
+  geom_point(aes(color = .cluster, shape = .cluster)) +
+  # geom_text_repel(aes(label = Country, color = .cluster), 
+  #                 size = 3, max.overlaps = 203897450, show.legend = FALSE) +
+  # Add centroid labels to plot
+  # geom_label(data = scatter_2020_kmeans4_summary, aes(label = cluster, color = cluster),
+  #            size = 3,
+  #            label.r = unit(0.5, "lines"),
+  #            label.size = 1.5,
+  #            label.padding = unit(0.5, "lines"),
+  #            show.legend = FALSE) +
+  labs(x = "Total COVID Deaths",
+       y = "Freedom to make life choices score",
+       color = "Cluster",
+       shape = "Cluster") +
+  theme_classic()
+ggsave("img/freedom_death_cluster")
+
+
